527cf7b6b731fc9b8c94404af7f0ad4e4bd9b7f1
Luqman25 <adedokunluqman@gmail.com>
2022-02-16 17:23:58 +0100

    LogIn TestCase

---
+++ SeleniumSpecflowProject/SeleniumSpecflowProject/TestCaseLogIn.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumSpecflowProject
+{
+    class TestCaseLogIn
+    {
+        [Test]
+        public void WebDriverTest()
+        {
+            IWebDriver webDriver = new ChromeDriver();
+            webDriver.Navigate().GoToUrl("https://admin-demo.nopcommerce.com/login");
+            
+            webDriver.FindElement(By.Id("Email")).Clear();
+            webDriver.FindElement(By.Id("Email")).SendKeys("admin@yourstore.com");
+            
+            webDriver.FindElement(By.Id("Password")).Clear();
+            webDriver.FindElement(By.Id("Password")).SendKeys("admin");
+            webDriver.FindElement(By.ClassName("buttons")).Click();
+        }
+    }
+}
