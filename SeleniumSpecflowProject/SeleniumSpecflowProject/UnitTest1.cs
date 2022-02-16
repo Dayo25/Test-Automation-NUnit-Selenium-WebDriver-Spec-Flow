ca699f8ea547e7cfe4d902fda63c901b41fffd28
Luqman25 <adedokunluqman@gmail.com>
2022-02-16 15:33:43 +0100

    NUnit Freamework Added

---
+++ SeleniumSpecflowProject/SeleniumSpecflowProject/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace SeleniumSpecflowProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
